316c12fcc0b178f74aa850a08b430b1e0c1bf62e
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2022-10-11 01:14:48 +1100

    pointer

--- main.go
+++ main.go
@@ -3,7 +3,31 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Welcome to our conference booking application")
-	fmt.Println("Get your ticket")
+	conferenceName := "Go Conference"
+	fmt.Println(conferenceName)
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+
+	
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+	
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend!")
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+
+	//fmt.Scan(&userName)
+	fmt.Println(remainingTickets)
+
+	fmt.Println(&remainingTickets)
+	
+	
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+
+
 }
 
